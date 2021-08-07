32d6497560122f72a08480afda35c6379c627eb5
LimeFinance <limefinance0@gmail.com>
2021-08-07 09:11:56 -0300

    release date changed

--- src/pages/Home/countdown.tsx
+++ src/pages/Home/countdown.tsx
@@ -1,6 +1,6 @@
 import Countdown from "react-countdown";
 
-const RELEASE_DATE = new Date("August 7, 2021 11:04:00");
+const RELEASE_DATE = new Date("August 7, 2021 12:20:00");
 
 const renderer = ({ days, hours, minutes, seconds, completed }) => {
   if (completed) {
